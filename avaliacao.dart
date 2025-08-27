e323ab46483b3f943475b3a897bdaa2ebbd7b3ed
Evellyn Ribeiro Sene Sim�es <54590135850@>
2025-08-27 09:03:01 -0300

    finalizada

--- avaliacao.dart
+++ avaliacao.dart
@@ -45,11 +45,15 @@ void main() {
 
   }while(continuar != null && continuar == 'sim' );
 
+  
+
   print("Escolha a forma de pagamento: ");
   int pagamento = 0;
-  int total = 0;
+  double valorTotal = calcularTotal(itensEscolhidos, servicos, precos);
   double desconto = 0;
   double valorAtualizado = 0;
+
+  bool pagamentoValido = false;
 
   do{
     print("1-Dinheiro");
@@ -60,10 +64,61 @@ void main() {
     pagamento = int.parse(stdin.readLineSync()!);
     switch(pagamento){
       case 1:
-        desconto = total * 0.10;
-        valorAtualizado = total - desconto;
+        desconto = valorTotal * 0.10;
+        valorAtualizado = valorTotal - desconto;
+
+        print("Total com desconto: R\$ $valorAtualizado");
+
+        bool pagamentoDinheiroValido = false;
+
+        while (!pagamentoDinheiroValido) {
+          print("Informe o valor pago pelo cliente: ");
+          double valorPago = double.parse(stdin.readLineSync()!);
+
+          double troco = valorPago - valorAtualizado;
+          if (troco < 0){
+            print("Valor pago insuficiente! Faltam R\$ ${-troco}");
+          } else {
+            print("Troco: R\$ $troco");
+            pagamentoDinheiroValido = true;
+            pagamentoValido = true;
+          }
+        }
+        break;
+
+      case 2:
+        pagamentoValido = true;
+        desconto = 0;
+        valorAtualizado = valorTotal;
+
+        print("Total a pagar (débito): R\$ $valorAtualizado");
+        break;
+      case 3:
+        pagamentoValido = true;
+        double juros = valorTotal * 0.10;
+        valorAtualizado = valorTotal + juros;
+
+        print("Total com juros (crédito): R\$ $valorAtualizado");
+        break;
+      case 4:
+        pagamentoValido = true;
+        desconto = valorTotal * 0.05;
+        valorAtualizado = valorTotal - desconto;
+
+        print("Total com desconto (PIX): R\$ $valorAtualizado");
+        break;
+
+      default:
+        print("Forma de pagamento inválida");
     }
+  }while (!pagamentoValido);
+
+  print("--RECIBO FINAL--");
+  print("Serviços escolhidos: ");
+  for (String item in itensEscolhidos){
+    print("- $item");
   }
+  print("Total a pagar: R\$ $valorAtualizado");
 }
 
 void menu(){
@@ -72,3 +127,20 @@ void menu(){
   print("3-Corte degrade");
   print("4-Corte elaborado");
 }
+
+double calcularTotal(List<String> itensEscolhidos, List<String> servicos, List<double> precos){
+    double total = 0;
+
+    for (int i = 0; i < itensEscolhidos.length; i++){
+      String item = itensEscolhidos[i];
+
+      for (int j = 0; j < servicos.length; j++ ){
+        if (item == servicos[j]){
+          total += precos[j];
+          break;
+        }
+      }
+    }
+
+    return total;
+  }
